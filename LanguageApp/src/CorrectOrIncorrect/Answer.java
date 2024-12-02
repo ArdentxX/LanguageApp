7a5a8929e6ced5f8589fce47f7a26caf0c1f285c
Kacper <lui3pytoficial@gmail.com>
2024-12-02 20:42:02 +0100

    LanguageApp Basic Functions(JSON Questions with options)

--- LanguageApp/src/CorrectOrIncorrect/Answer.java
+++ LanguageApp/src/CorrectOrIncorrect/Answer.java
@@ -1,19 +1,11 @@
 package CorrectOrIncorrect;
-
-import Questions.Questions;
-
 public class Answer {
     protected String getanswers;
-
-
-    public Answer() {
-    }
+    public Answer() {}
     public void setAnswer(String getanswers) {
         this.getanswers = getanswers;
     }
     public String getAnswer() {
         return getanswers;
     }
-
-
 }
